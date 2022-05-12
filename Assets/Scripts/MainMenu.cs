6efd2379c5872db9f55bf1cba1390cc01791edf7
Utkan Çelik <utkancelik@hacettepe.edu.tr>
2022-05-12 21:51:06 +0300

    Added HighScore system

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TMP_Text HighScoreText;
+    private void Start()
     {
-        
+        HighScoreText.text = "Highscore : "+PlayerPrefs.GetInt(ScoreSystem.HighScoreKey).ToString();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void LoadScene_Game()
     {
-        
+        // Loading the game screen in index 1
+        SceneManager.LoadScene(1);
     }
 }
--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -8,6 +8,7 @@ public class ScoreSystem : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplier = 1.15f;
 
+    public const string HighScoreKey = "HighScore";
 
     private float score;
 
@@ -16,5 +17,15 @@ public class ScoreSystem : MonoBehaviour
         score += (Time.deltaTime * scoreMultiplier);
 
         scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
     }
 }
